fc9446298dc493154b5becb2f07959118320de1e
AYUSH PATEL <110957486+Ayush1350@users.noreply.github.com>
2022-09-14 14:46:07 +0530

    Explicit intent

---
+++ app/src/main/java/com/example/explicit/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.explicit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.accessibility.AccessibilityManagerCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mMsgEditText: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mMsgEditText=findViewById(R.id.textmsg)
+    }
+
+    fun lauchsecondactivity(view: View) {
+    Toast.makeText(this,"send button Clicked",Toast.LENGTH_LONG).show()
+        Log.i("MainActivity","button is speaking")
+       var intent = Intent(this,SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+}

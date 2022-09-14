e4274b9fff0c3406ee1f3c485d99243205a1bda9
AYUSH PATEL <110957486+Ayush1350@users.noreply.github.com>
2022-09-14 15:12:44 +0530

    Explicit intent

--- app/src/main/java/com/example/explicit/MainActivity.kt
+++ app/src/main/java/com/example/explicit/MainActivity.kt
@@ -16,12 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mMsgEditText=findViewById(R.id.textmsg)
+
     }
 
     fun lauchsecondactivity(view: View) {
     Toast.makeText(this,"send button Clicked",Toast.LENGTH_LONG).show()
         Log.i("MainActivity","button is speaking")
+        var msg =mMsgEditText.text.toString()
        var intent = Intent(this,SecondActivity::class.java)
+
+
+        intent.putExtra("Msg",msg)
         startActivity(intent)
     }
 
--- app/src/main/java/com/example/explicit/SecondActivity.kt
+++ app/src/main/java/com/example/explicit/SecondActivity.kt
@@ -1,11 +1,21 @@
 package com.example.explicit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var  mTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        mTextView = findViewById(R.id.text_Second)
+
+        val intent = intent
+            var m1:String??=intent.getStringExtra("msg")
+
     }
 }
